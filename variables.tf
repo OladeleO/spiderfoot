f1b37e4f37d63fb9277c08b26620f5e5175898ba
OladeleO <58373625+OladeleO@users.noreply.github.com>
2021-09-23 09:18:11 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "project" {
+  default = "mythic-inn-323010"
+}
+
+variable "region" {
+  default = "europe-west1"
+}
+
+variable "zone" {
+  default = "europe-west1-b"
+}
+
+variable "cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "gke_cluster_name" {
+  default = "padsop-cluster-spiderfoot"
+}
